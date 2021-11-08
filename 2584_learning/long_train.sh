6b7ad10ced0c0417aa6f4fd3950de5be0dd1d1bd
FrzWilly <k.willy006@gmail.com>
2021-11-08 22:04:39 +0800

    return to use 8x4 tuple 2 5-tuple and 1 6-tuple and update long train script

--- 2584_learning/long_train.sh
+++ 2584_learning/long_train.sh
@@ -1,8 +1,8 @@
-```bash
-./2048 --total=0 --play="init save=weights.bin" # generate a clean network
+```zsh
+./2584 --total=0 --play="init save=weights.bin" # generate a clean network
 for i in {1..100}; do
-	./2048 --total=100000 --block=1000 --limit=1000 --play="load=weights.bin save=weights.bin alpha=0.0025" | tee -a train.log
-	./2048 --total=1000 --play="load=weights.bin alpha=0" --save="stat.txt"
+	./2584 --total=100000 --block=1000 --limit=1000 --play="load=weights.bin save=weights.bin alpha=0.0035714" | tee -a train.log
+	./2584 --total=1000 --play="load=weights.bin alpha=0" --save="stat.txt"
 	tar zcvf weights.$(date +%Y%m%d-%H%M%S).tar.gz weights.bin train.log stat.txt
 done
 ```
